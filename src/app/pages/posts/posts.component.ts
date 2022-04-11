aa879ba635fbb676f5999db9aba53c01d4eb3747
Alberto Perales <alberto.peraleslafuente@everis.nttdata.com>
2022-04-11 12:19:01 +0200

    Video 26

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  mensajes: any;
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log('INIT!');
+
+     this.mensajes = this.dataService.getPost()
+    // .subscribe(( posts: any ) => {
+    //   console.log(posts);
+    //   this.mensajes = posts;
+    // });
   }
-
 }
